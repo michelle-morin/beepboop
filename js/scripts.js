8fffe3669bf80188249ce7b90dbf2589ecc91bf0
Michelle Morin <morin2m@gmail.com>
2020-01-24 10:00:52 -0800

    edit makeListOfNumbers function to include additional argument of a userName, and add conditional statement to front-end logic to handle cases in which a user does not enter a name

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Back-end (business) logic:
 
-function makeListOfNumbers(number) {
+function makeListOfNumbers(number, userName) {
   var numbers = [];
   for (var index = 0; index <= number; index ++) {
     numbers.push(index);
   }
   for (var i = 0; i <= number; i ++) {
     if (i.toString().includes("3")) {
-      numbers[i] = "I'm sorry, Dave. I'm afraid I can't do that."
+      numbers[i] = "I'm sorry, " + userName + ". I'm afraid I can't do that.";
     } else if (i.toString().includes("2")) {
-      numbers[i] = "Boop!"
+      numbers[i] = "Boop!";
     } else if (i.toString().includes("1")) {
-      numbers[i] = "Beep!"
+      numbers[i] = "Beep!";
     }
   }
   return numbers;
@@ -27,6 +27,9 @@ $(document).ready(function() {
 
     var userInputNumber = parseInt($("input#number").val());
     var userName = $("input#name").val();
+    if (!userName) {
+      userName = "Dave";
+    }
 
     if (isNaN(userInputNumber)) {
       $("#output h3").hide();
@@ -34,7 +37,7 @@ $(document).ready(function() {
     } else {
       $("#output h3").show();
       $(".input-number").text(userInputNumber);
-      var listOfNumbers = makeListOfNumbers(userInputNumber);
+      var listOfNumbers = makeListOfNumbers(userInputNumber, userName);
       listOfNumbers.forEach(function(number) {
         $("ul#result").append("<li>" + number + "</li>");
       });
