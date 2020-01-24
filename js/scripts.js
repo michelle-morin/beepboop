53864f9fe3da45402bf503cef93e18ceeb68cfd8
Michelle Morin <morin2m@gmail.com>
2020-01-24 09:12:34 -0800

    update back-end logic to handle multi-digit input numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ function makeListOfNumbers(number) {
     numbers.push(index);
   }
   for (var i = 0; i <= number; i ++) {
-    if (i === 3) {
+    if (i.toString().includes("3")) {
       numbers[i] = "I'm sorry, Dave. I'm afraid I can't do that."
-    } else if (i === 2) {
+    } else if (i.toString().includes("2")) {
       numbers[i] = "Boop!"
-    } else if (i === 1) {
+    } else if (i.toString().includes("1")) {
       numbers[i] = "Beep!"
     }
   }
@@ -23,11 +23,10 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $("#output").show();
+    $("ul#result").empty();
 
     var userInputNumber = parseInt($("input#number").val());
     var userName = $("input#name").val();
-
-    $("ul#result").empty();
 
     if (isNaN(userInputNumber)) {
       $("#output h3").hide();
@@ -35,9 +34,7 @@ $(document).ready(function() {
     } else {
       $("#output h3").show();
       $(".input-number").text(userInputNumber);
-
       var listOfNumbers = makeListOfNumbers(userInputNumber);
-  
       listOfNumbers.forEach(function(number) {
         $("ul#result").append("<li>" + number + "</li>");
       });
